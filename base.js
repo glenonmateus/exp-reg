c8ad26e6d83393f059b3d439106907bff8321dd1
Glenon Mateus <glenonmateus+github@gmail.com>
2026-08-09 11:48:59 -0300

    feat: add texto

---
+++ base.js
@@ -0,0 +1,10 @@
+export const texto = `
+João trouxe flores para sua amada namorada em 10 de janeiro de 1970,
+Maria era o nome dela.
+
+Foi um ano excelente na vida de João. Teve 5 filhos, todos adultos atualmente.
+maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de
+doming. Também né! Sendo a boa mineira que é, nunca esquece seu famoso pão de queijo.
+Não canso de ouvir a Maria:
+"Jooooooooãoooooo, o café tá prontinho aqui, Veeemm!"
+`
